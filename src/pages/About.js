6619011f722c8641b17e61e6902b304009910124
ogneyar <ogneyar-ne@ya.ru>
2021-02-01 21:41:29 +0300

    add baner skyway

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,20 @@
+import './Home.css';
+
 import React from 'react';
 
 export const About = () => {
     return (
         <>
-            <h1>О нас</h1>
+            
+            <img src="/video/about-yarga.gif" alt="about-yarga" />
+
+
+            {/* <h1>О нас</h1> */}
+
+            {/* <video autoplay>
+                <source src="/video/about-yarga.mp4" type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"' />
+            </video> */}
+
         </>
     );
 }
